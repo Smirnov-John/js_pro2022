5b8db77c495ce75fce3833d93e90c3ebfeffc309
Smirnov-John <smirnovevvi@mail.ru>
2023-02-05 17:46:37 +0300

    Лекция 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
 import React from 'react';
-// import logo from './logo.svg';
-import { CounterComponent } from './CounterComponent';
 import './App.css';
 import { Input } from './Input';
 
 function App() {
-  const handler = () => {
-    console.log('Кнопка была нажата');
-  };
   return (
-    <div className="App">
-      <CounterComponent title={'Orange'} />
-      <CounterComponent title={'Apple'} count={4} />
-      <CounterComponent title={'Lemon'} />
-      <button onClick={handler}> PUSH ME</button>
-
-      <Input />
-    </div>
   );
 }
 
